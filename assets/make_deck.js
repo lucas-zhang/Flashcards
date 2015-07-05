1e4c858dba585349485ba39e92344dffd1c65469
Lucas Zhang <lucas.zhang@berkeley.edu>
2015-07-05 18:53:54 -0500

    add and delete cards

--- assets/make_deck.js
+++ assets/make_deck.js
@@ -8,11 +8,21 @@ $(document).ready(function(){
     $("#next-button").click(function(){
         curr = $(window).scrollTop();
         dest = curr + 160;
-        var newRow = "<div style = 'display: none' class='card-row'><textarea class = 'card-text'></textarea><textarea class = 'card-text'></textarea></div>";
+        var newRow = "<div style = 'display: none' class='card-row'><img src = 'assets/img/x-icon.png' class = 'exit-img'><textarea class = 'card-text'></textarea><textarea class = 'card-text'></textarea></div>";
         $(newRow).appendTo("#card-space").fadeIn('slow');
         $('body, html').animate({scrollTop: dest}, 450);
         $("#card-space").children(":last-child").children(":first-child").focus();
     
+    });
+    var deletedRows = [];
+
+    $(document).on("click", ".exit-img", function() {
+        $(this).parent().fadeOut(500);
+        deletedRows.push($(this).parent());
+    });
+
+    $("#undo-button").click(function(){
+        deletedRows.pop().fadeIn(600);
     });
 
 
